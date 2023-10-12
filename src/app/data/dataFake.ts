51322acafe8ee611deed004a80a50475f3592103
Abraão Mendes <abraaomneto@gmail.com>
2023-10-11 21:39:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Tite no flamengo já é realidade!",
+        "description": "O novo comandante chegou ao Flamengo: Tite chegou ao clube na manhã desta terça-feira (10). O treinador apareceu no momento em que Ninho do Urubu começou a trabalhar e posou para fotos com a camisa vermelha e preta.",
+        "photoCover":"https://th.bing.com/th/id/OIF.SLtPDsMueEAif6myFpxB9Q?pid=ImgDet&rs=1"
+    },
+    {
+        "id":"2",
+        "title": "Novo camisa 7 da seleção brasileira",
+        "description": "bla bla bla",
+        "photoCover":"https://uploads.metropoles.com/wp-content/uploads/2022/09/16195133/vini-jr-real-madrid-racismo.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Paquetá jogando mundo pelo West Ham",
+        "description": "bla bla bla",
+        "photoCover":"https://th.bing.com/th/id/R.84f2a2d6e2eec9c2e45382d9daaa3153?rik=2jDfrslRNNY7Xg&pid=ImgRaw&r=0"
+    },
+    {
+        "id":"4",
+        "title": "Neymar considerado o homem mais fiel do planeta",
+        "description": "bla bla bla",
+        "photoCover":"https://static.highsnobiety.com/thumbor/BbHLIDj1u7K6goY_HJeQ3GTi-Uc=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/08/31105808/neymar-nike-deal-part-ways-01.jpg" 
+    }
+
+]
